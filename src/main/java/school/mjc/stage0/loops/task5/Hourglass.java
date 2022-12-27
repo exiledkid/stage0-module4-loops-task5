49ab5aeb48c9952a39bdbc3c29d3e379bc260823
exiledkid <fortunemodspartner@gmail.com>
2022-12-27 23:06:16 +0300

    ...

--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int num = height;
 
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height; j++) {
+
+                if (i == 0 || i == height) {
+                    System.out.print(8);
+                } else if (j >= i && j <= num) {
+                    System.out.print(8);
+                } else if (j <= i && j >= num) {
+                    System.out.print(8);
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+            num--;
+        }
     }
 }
